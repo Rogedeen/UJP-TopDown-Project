bd163fb8c1037fb8b309b4d19e2415c70b319c0e
Rogedeen <ynsisikgoz@gmail.com>
2026-02-01 16:23:53 +0300

    Add Barrier prefab and material
    Introduced a new Barrier prefab with associated material and updated the MainScene to use the Barrier. Added collision logic in Gates.cs to deactivate gates when colliding with a Barrier. Removed the Tree prefab and its references.

--- Assets/Scripts/Game Runners/Gates.cs
+++ Assets/Scripts/Game Runners/Gates.cs
@@ -4,4 +4,15 @@ public class Gates : MonoBehaviour
 {
     public bool isActive;
 
+
+
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Barrier")) 
+        {
+            isActive = false;
+        }
+    }
 }
